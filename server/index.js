f3708e595564859b2eee0f1127c2336dc0e9c43e
Md Fahad Hossain <fahadhossainkhan@gmail.com>
2026-03-01 14:29:13 +0600

    Implement user CRUD operations and set up routing

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import route from "./routes/userRoute.js";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 7000;
@@ -15,7 +18,9 @@ mongoose
   .then(() => {
     console.log("DB connected successfully.");
     app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
+      console.log(`Server is running on port :${PORT} `);
     });
   })
   .catch((error) => console.log(error));
+
+app.use("/api", route);
